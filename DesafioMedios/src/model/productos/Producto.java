d71c9981861bf73f52c99e1b106bc11d83c1b38a
Harttyn Arce <harttyn.arce@gmail.com>
2020-06-23 21:30:26 -0400

    save slot 1

--- DesafioMedios/src/model/productos/Producto.java
+++ DesafioMedios/src/model/productos/Producto.java
@@ -1,4 +1,57 @@
 package model.productos;
 
 public abstract class Producto {
+    private int precio;
+    private String nombre;
+    private String codigo;
+    private int talla;
+    private String marca;
+
+    public Producto(int precio, String nombre, String codigo, int talla, String marca) {
+        this.precio = precio;
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.talla = talla;
+        this.marca = marca;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getTalla() {
+        return talla;
+    }
+
+    public void setTalla(int talla) {
+        this.talla = talla;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
--- DesafioMedios/src/model/productos/Zapato.java
+++ DesafioMedios/src/model/productos/Zapato.java
@@ -1,4 +1,19 @@
 package model.productos;
 
-public class Zapato {
+public class Zapato extends Producto{
+
+    private String modelo;
+
+    public Zapato(int precio, String nombre, String codigo, int talla, String marca, String modelo) {
+        super(precio, nombre, codigo, talla, marca);
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 }
